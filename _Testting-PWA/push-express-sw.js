d07a61bc6da1ddf50bb34077519b82cb2c59fb57
Anton Dev <anton.pwa@traffit.team>
2024-09-11 11:52:49 +0300

    fix

--- _Testting-PWA/push-express-sw.js
+++ _Testting-PWA/push-express-sw.js
@@ -1,1 +1,11 @@
-importScripts('https://sdk.push.express/js/v1.2.0/push-express-sdk.js');
+const firebaseConfig = {
+    apiKey: "AIzaSyAlgEDiuNWaPBhEWiij3Ho8OW-2f1wJnB0",
+    authDomain: "pwa-push-c85c4.firebaseapp.com",
+    projectId: "pwa-push-c85c4",
+    storageBucket: "pwa-push-c85c4.appspot.com",
+    messagingSenderId: "924760227755",
+    appId: "1:924760227755:web:76ff2394c8ba28e3c19cee",
+    measurementId: "G-RHG8WH8PXE"
+  };
+const push_express_app_id = "25121-1086";
+importScripts('https://sdk.push.express/js/v1.2.1/push-express-sw.js');
